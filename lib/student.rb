e9847c7005882536c7673362bf8063e077098007
Nick Hall <nickhall122@gmail.com>
2017-09-26 16:55:12 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+class Student < User
+  attr_reader :knowledge, :teach, :learn
+@knowledge = []
 
+  def initialize
+    @knowledge = []
+
+  end
+
+  def teach
+    number = rand(0..@knowledge.length)
+    @knowledge[number]
+  end
+
+  def learn(string)
+    @knowledge << string
+  end
 end
--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,21 @@
-class Teacher
+require 'pry'
 
+class Teacher < User
+ #attr_reader :knowledge
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
-  
+  # def initialize
+  #   @knowledge = []
+  #
+  # end
+  #
+  # def learn(string)
+  #   @knowledge << string
+  # end
+  def teach
+    #binding.pry
+    number = rand(0..KNOWLEDGE.length)
+    KNOWLEDGE[number]
+  end
+
 end
